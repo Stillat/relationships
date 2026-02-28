85404eb545f65c017bbfd6440cc634575abf2937
John Koster <john@stillat.com>
2026-02-28 13:36:08 -0600

    Unbork the tests

--- tests/BaseTestCase.php
+++ tests/BaseTestCase.php
@@ -18,7 +18,6 @@ abstract class BaseTestCase extends \Orchestra\Testbench\TestCase
 
         if ($this->shouldFakeVersion) {
             \Facades\Statamic\Version::shouldReceive('get')->andReturn('3.0.0-testing');
-            $this->addToAssertionCount(-1); // Dont want to assert this
         }
 
         // Boot our Addon's events so those work :)
